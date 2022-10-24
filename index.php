b35d01f1ce5a9e8e85296efbb709c484bf6d297b
Satria Yudha Pratama <yudhap452@gmail.com>
2022-10-24 11:14:48 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World! <br>";
+
+$nama = "Satria Yudha Pratama";
+$umur = "21";
+
+echo "Nama saya <strong>$nama</strong>, saya berumur $umur tahun <br>";
+
+$namaAdik = "Laily Cahaya Putri";
+$umurAdik = "16";
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berumur $umurAdik tahun <br>";
+
+$selisihUmur = $umur - $umurAdik;
+
+echo "Selisih umur saya dengan adik saya adalah $selisihUmur tahun";
+
+?>
